f670d125e553c0e0b6c3dcf3837bb8d146610357
张营磊 <zhyinglei@szlanyou.com>
2022-09-09 16:06:20 +0800

    add BFS

--- src/Graph.h
+++ src/Graph.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <unordered_set>
 #include <unordered_map>
+#include <queue>
 
 using std::cout;
 
@@ -10,21 +11,66 @@ class Graph
 {
 private:
     int count_;  //¶¥µã¸öÊý
-    unordered_map<int, int> adjMap_;
+    vector<list<int>> adjList_;
 
 public:
     Graph(const int count)
     :count_(count)
     {
-        adjMap_.reserve(count);
+        adjList_.resize(count_);
     }
 
     ~Graph() = default;
 
     void addEdge(const int i, const int j)
     {
-        adjMap_[i] = j;
-        adjMap_[j] = i;
+        adjList_[i].push_back(j);
+        adjList_[j].push_back(i);
     }
 
+    void bfs(const int start, const int target)
+    {
+        if (start == target) return;
+
+        vector<bool> vecVisited;
+        vecVisited.resize(count_);
+        vecVisited[start] = true;
+
+        queue<int> queQueue;
+        queQueue.push(start);
+
+        vector<int> vecPrev;
+        vecPrev.resize(count_);
+
+        while (!queQueue.empty()) {
+            int w = queQueue.front(); queQueue.pop();
+
+            for (auto elment : adjList_[w]){
+                if (!vecVisited[elment]) {
+                    vecPrev[elment] = w;
+
+                    if (elment == target)
+                    {
+                        print(vecPrev, start, target); cout << endl;
+                        return;
+                    }
+
+                    vecVisited[elment] = true;
+                    queQueue.push(elment);
+                }
+            }
+        }
+    }
+
+private:
+
+    void print(const vector<int>& p_vecPrev, const int start, int target)
+    {
+        if (p_vecPrev[target] != -1 && start != target) {
+            target--;
+            print(p_vecPrev, start, p_vecPrev[target]);
+        }
+
+        cout << target + " ";
+    }
 };
