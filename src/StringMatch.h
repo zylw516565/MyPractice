a9b36656a31c02d76917f30d752fdfb5c35c54fb
张营磊 <zhyinglei@szlanyou.com>
2022-09-21 15:32:03 +0800

    BM算法:增加填充suffix,prefix方法generateGS

--- src/StringMatch.h
+++ src/StringMatch.h
@@ -113,6 +113,29 @@ public:
         }
     }
 
+    void generateGS(const string& pattern, vector<int>& suffix, vector<bool>& prefix)
+    {
+        if (pattern.empty()) {
+            return;
+        }
+
+        int nPatternLen = pattern.size();
+        suffix.resize(nPatternLen, -1);
+        prefix.resize(nPatternLen, false);
+        for (int i = 0; i < nPatternLen - 1; ++i)  //b[0, i]
+        {
+            int j = i;  //b[0, i] 从尾部与b[0, m-1]求公共后缀子串
+            int k = 0;  //公共后缀子串长度
+            while (j >= 0 && pattern[j] == pattern[nPatternLen - 1 - k]) {  // 与b[0, m-1]求公共后缀子串
+                --j;
+                ++k;
+                suffix[k] = j + 1;
+            }
+
+            if (j == -1) prefix[k] = true;
+        }
+    }
+
 private:
 
 
